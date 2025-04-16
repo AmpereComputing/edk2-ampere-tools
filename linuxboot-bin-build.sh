880dfffd2a32f30883a7e25f9755facbdce74297
Nhi Pham <npham@amperecomputing.com>
2025-04-16 14:33:30 +0700

    linuxboot-bin-build.sh: Fix build error
    The build script is broken by some changes from the upstream LinuxBoot
    repo. This fixes it and upgrades the golang compiler to version 1.24.2
    to support building with a modern Linux distro.
    
    Signed-off-by: Nhi Pham <npham@amperecomputing.com>

--- linuxboot-bin-build.sh
+++ linuxboot-bin-build.sh
@@ -8,7 +8,7 @@
 #
 # LinuxBoot Binary Build
 #
-GOLANG_VER=1.20.4
+GOLANG_VER=${GOLANG_VER:-1.24.2}
 TOOLS_DIR="`dirname $0`"
 TOOLS_DIR="`readlink -f \"$TOOLS_DIR\"`"
 export TOOLS_DIR
@@ -23,8 +23,6 @@ fi
 
 check_golang ${GOLANG_VER}
 export GOPATH=${TOOLS_DIR}/toolchain/gosource
-export GOFLAGS=-modcacherw
-export GO111MODULE=auto
 mkdir -p ${GOPATH}
 export PATH=${GOPATH}/bin:${TOOLS_DIR}/toolchain/go/bin:$PATH
 
@@ -43,19 +41,8 @@ if [ -d ${LINUBOOT_DIR}/mainboards/ampere/${PLATFORM_LOWER}/linux ]; then
     make -C ${LINUBOOT_DIR}/mainboards/ampere/${PLATFORM_LOWER}/linux distclean
 fi
 LINUBOOT_MAKEFILE=${LINUBOOT_DIR}/mainboards/ampere/${PLATFORM_LOWER}/Makefile
-if ! grep -q "uroot-source" "${LINUBOOT_MAKEFILE}"; then
-    sed -i "s;uinitcmd=systemboot;uinitcmd=systemboot -uroot-source \$\{GOPATH\}/src/github.com/u-root/u-root;" ${LINUBOOT_MAKEFILE}
-fi
 
-if  grep -q "GO111MODULE=off" "${LINUBOOT_MAKEFILE}"; then
-    sed -i "s;GO111MODULE=off;;" ${LINUBOOT_MAKEFILE}
-fi
-
-go get -d github.com/u-root/u-root
-go get -d github.com/u-root/cpu/...
-go install github.com/u-root/u-root@v0.9.0
-
-make -C $LINUBOOT_DIR/mainboards/ampere/${PLATFORM_LOWER} getkernel flashkernel ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE}
+make -C $LINUBOOT_DIR/mainboards/ampere/${PLATFORM_LOWER} fetch flashkernel ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE}
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
     echo "ERROR: compile LinuxBoot binaries issue" >&2
